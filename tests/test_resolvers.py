db6a0bd838834e4da412f7db82eb2fb55e324c17
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-01-02 16:15:32 -0500

    Minor syntax fix.

--- tests/test_resolvers.py
+++ tests/test_resolvers.py
@@ -42,7 +42,7 @@ WIKI_EXAMPLES = [
 
 WIKI_EXAMPLES_RX =[
     ('(get "wikipedia-mountain" "Mount Everest" (:code "ELEVATION_M"))',
-     r'^\(\(:html "8848"\)\)$')
+     r'^\(\(:html "8848"\)\)$'),
     ('(get "wikipedia-military-conflict" "American Civil War" (:code "RESULT"))',
      r'<ul>.*<li>.*<\/ul>')]
 
