461c3e5e97c38b357d219006fa0780d041a78a86
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-02-17 13:03:22 +0200

    Regular expression builder

---
+++ wikipediabase/rxbuilder.py
@@ -0,0 +1,138 @@
+# Requirements:
+# - Operations on regex: sequential/separated, or
+# - Exclusivity in naming.
+
+def rxf(rx):
+    if not isinstance(rx, Regex):
+        return Regex(rx)
+
+    return rx
+
+# XXX: all methods that receve banned_names are destructive to it.
+class Regex(object):
+
+    # Note that rx can be None in subclasses.
+    def __init__(self, rx=None, name=None, grouped=False, flags=None):
+        self.rx = rx or ""
+        self.grouped = grouped
+        self.name = name
+        self.flags = None
+
+    def _format(self, banned_names):
+        # XXX: this has the side effect of adding to banned
+        # names. This means that a named is sure to be there only
+        # once.
+        fmt = "%s"
+        if self.grouped:
+            fmt = "(%s)"
+
+        if self.name and self.name not in banned_names:
+            banned_names.append(self.name)
+            fmt = "(?<"+self.name+">%s)"
+
+        return fmt
+
+    def render(self, banned_names=None, raw=False):
+        """
+        Render the regex.
+
+        :param banned_names: A list of names that are banned. if the name
+        is among those do not name the rx.
+
+        :returns: String of what this regex means.
+        """
+
+        if banned_names is None:
+            banned_names = []
+
+        if raw:
+            fmt = "%s"
+        else:
+            fmt = self._format(banned_names)
+
+        s = self.string(banned_names)
+
+        return fmt % s
+
+    def string(self, banned_names=None):
+        if isinstance(self.rx, Regex):
+            return self.rx.render(banned_names)
+
+        return self.rx
+
+    def __str__(self):
+        return self.render()
+
+    def compiled(self, flags=None):
+        return re.compile(str(self), flags or self.flags)
+
+    def __nonzero__(self):
+        return True if self.render(raw=True) != "" else False
+
+    def __add__(self, rx):
+        """
+        A concatenated version of rx.
+        """
+
+        return ConcatRx(self, rx)
+
+    def __or__(self, rx):
+        return OrRx(self, rx)
+
+
+class RxExpr(Regex):
+    """
+    In case of name conflict the name is taken by the shallowest and
+    rightmost.
+    """
+
+    def __init__(self, left, right, sep="", **kw):
+        self.sep = rxf(sep)
+        self.left = rxf(left)
+        self.right = rxf(right)
+
+        super(RxExpr, self).__init__(**kw)
+
+    def string(self, banned_names=None, **kw):
+        if banned_names is None:
+            banned_names = []
+
+        r,s,l = [i.render(banned_names, **kw) if i else "" \
+                 for i in [self.right, self.sep, self.left]]
+
+        return l+s+r
+
+
+class OrRx(RxExpr):
+
+    def __init__(self, *args, **kwargs):
+        lrx = args[0]
+        kwargs['sep'] = '|'
+        grp = kwargs["grouped"] if 'grouped' in kwargs else True
+        kwargs['grouped'] = False
+
+        if len(args) > 2:
+            rrx = OrRx(*args[1:], **kwargs)
+        elif len(args) == 2:
+            rrx = args[1]
+        else:
+            rrx = ""
+
+        kwargs['grouped'] = grp
+        super(OrRx, self).__init__(lrx, rrx, **kwargs)
+
+class ConcatRx(RxExpr):
+
+    def __init__(self, *args, **kwargs):
+        if len(args) == 0:
+            return super(ConcatRx, self).__init__("", "", "", **kwargs)
+
+        lrx = args[0]
+        if len(args) > 2:
+            rrx = ConcatRx(*args[1:], **kwargs)
+        elif len(args) == 2:
+            rrx = args[1]
+        else:
+            rrx = ""
+
+        super(ConcatRx, self).__init__(lrx, rrx, **kwargs)
