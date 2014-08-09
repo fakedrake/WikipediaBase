03f8a6d065f3c70b17dc2db4e718c0dc08934529
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-08-09 03:13:01 +0300

    Useful untested utilities for direct databaes a cess/

--- wikipediabase/dbfetcher.py
+++ wikipediabase/dbfetcher.py
@@ -27,5 +27,44 @@ class DBFetcher(BaseFetcher):
         self.cur.execute(cmd)
         return self.cur.fetchall()
 
+    def _query_single(self, cmd):
+        return int(self._query_one_raw(cmd)[0])
+
+    def _query_single_list(self, cmd):
+        return (i[0] for i in self._query_raw(cmd))
+
     def tables(self):
-        return [i[0] for i in self._query_raw("show tables;")]
+        return list(self._query_single_list("show tables;"))
+
+    def columns(self, table):
+        return list(self._query_single_list("show columns from %s;" % table))
+
+    def n_categories(self):
+        cmd = "select count(*) from page where page_namespace = 14;"
+        return int(self._query_single(cmd))
+
+    def categories(self):
+        cmd = "select page_id, page_title from page where page_namespace = 14;"
+        return ({'id': id, 'name': title} for id, title
+                in self._query_raw(cmd))
+
+    def article_id(self, title):
+        cmd = "select page_id from page where page_title = '%s'" % title
+        return int(self._query_single(cmd))
+
+    def article_title(self, id):
+        cmd = "select page_title from page where page_id = '%s'" % id
+        return self._query_single(cmd)
+
+    def article_categories(self, article):
+        pass
+
+def get_dbfetcher(new=False, **kw):
+    try:
+        if not new:
+            return get_dbfetcher._fetcher
+    except AttributeError:
+        pass
+
+    get_dbfetcher._fetcher = DBFetcher(**kw)
+    return get_dbfetcher(**kw)
