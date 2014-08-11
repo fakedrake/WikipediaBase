db1f5e4d449d57c9fede3fcb55067e70cccd221f
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-08-11 04:51:08 +0300

    Extra commands and also I can save the categories.

--- wikipediabase/dbfetcher.py
+++ wikipediabase/dbfetcher.py
@@ -1,9 +1,10 @@
+from itertools import chain
+
 import MySQLdb as mdb
 
 from wikipediabase.fetcher import BaseFetcher
 
 class DBFetcher(BaseFetcher):
-
     _default_conn = {
         'user': "csail",
         'passwd': "pass",
@@ -28,7 +29,7 @@ class DBFetcher(BaseFetcher):
         return self.cur.fetchall()
 
     def _query_single(self, cmd):
-        return int(self._query_one_raw(cmd)[0])
+        return self._query_one_raw(cmd)[0]
 
     def _query_single_list(self, cmd):
         return (i[0] for i in self._query_raw(cmd))
@@ -56,15 +57,76 @@ class DBFetcher(BaseFetcher):
         cmd = "select page_title from page where page_id = '%s'" % id
         return self._query_single(cmd)
 
+    def all_articles(self, limit=None):
+        """
+        Get a list of tuples (page_id, page_text)
+        """
+
+        cmd = "select rev_page, old_text from revision " \
+              "inner join text on old_id = rev_text_id " \
+              "%s;" % ("limit %d" % limit if limit is not None else "")
+        return self._query_raw(cmd)
+
+    # XXX: there is no obvious way to just get the category ids
+    # traight from the data base so you might want to do you training
+    # with the strings.
     def article_categories(self, article):
-        pass
+        """
+        Find in the article text [[Category:.*]]
+        """
+
+        return self._categories(self.article_text(article))
+
+    def _categories(self, txt):
+        """
+        return the names of the categories.
+        """
+
+        # It is slightly faster like this because we are nto creating
+        # a lambda obj each time.
+        def first_part(s):
+            return s.split(']]', 1)[0]
+
+        return map(first_part, txt.split("[[Category:")[1:])
+
+    def all_article_categories(self, limit=None):
+        return chain.from_iterable((((id, cat) for cat in self._categories(txt) )
+                                    for id, txt in self.all_articles(limit)))
+
+    def article_text(self, article):
+        """
+        You need to jump from page(title)->revision->text
+        """
+
+        if isinstance(article, str):
+            cmd = "select old_text from page " \
+                  "inner join revision on rev_page = page_id " \
+                  "inner join text on old_id = rev_text_id " \
+                  "where page_title = '%s';" % article
+
+        else:                   # Assume it's an id
+            cmd = "select old_text from revision " \
+                  "inner join text on old_id = rev_text_id " \
+                  "where rev_page = '%d';" % article
+
+        return self._query_single(cmd)
+
 
 def get_dbfetcher(new=False, **kw):
-    try:
-        if not new:
+    if not new:
+        try:
             return get_dbfetcher._fetcher
-    except AttributeError:
-        pass
+        except AttributeError:
+            pass
 
     get_dbfetcher._fetcher = DBFetcher(**kw)
     return get_dbfetcher(**kw)
+
+
+def _main():
+    import json
+    fetcher = get_dbfetcher()
+    json.dump(list(fetcher.all_article_categories()), "~/Documets/article_categories.json")
+
+if __name__ == "__main__":
+    _main()
