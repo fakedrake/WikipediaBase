9f39ed242ba38fe5720440c25281516ff5691c64
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-08-15 08:13:50 +0300

    Test of first paragraph.

---
+++ tests/test_paragraph.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_paragraph
+----------------------------------
+
+Tests for `paragraph` module.
+"""
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from .common import get_fetcher
+
+from wikipediabase import wikipediabase
+from wikipediabase.frontend import Frontend
+
+class TestParagraph(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_first_paren(self):
+        self.assertEqual(wikipediabase('(get "Mary Shakespeare" "birth-date")',
+                                       fetcher=get_fetcher()),
+                         "((:yyyymmdd 15370000))")
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
