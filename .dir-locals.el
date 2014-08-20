d30f641a980b16dc21b214f706249b635aecfcb4
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-08-20 17:47:31 +0300

    Actually test

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,8 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil
-  (compilation-directory . "/home/fakedrake/Projects/CSAIL/Python/WikipediaBase/wikipediabase//"))
+  (compilation-directory . "/home/fakedrake/Projects/CSAIL/Python/WikipediaBase/wikipediabase//")
+  (compile-command . "/home/fakedrake/Projects/CSAIL/Python/py/bin/python  setup.py flake8"))
  (python-mode
   (python-shell-virtualenv-path . "/home/fakedrake/Projects/CSAIL/Python/py/")
   (compile-command . "/home/fakedrake/Projects/CSAIL/Python/py/bin/pep8 /home/fakedrake/Projects/CSAIL/Python/WikipediaBase/wikipediabase /home/fakedrake/Projects/CSAIL/Python/WikipediaBase/tests")
