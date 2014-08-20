bbaaff8b70ec28f0f2a3952a3476691912aa088e
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-08-20 21:03:24 +0300

    Dont require pinoccio

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,6 @@
   (compilation-directory . "/home/fakedrake/Projects/CSAIL/Python/WikipediaBase/wikipediabase//")
   (compile-command . "/home/fakedrake/Projects/CSAIL/Python/py/bin/python  setup.py flake8"))
  (python-mode
-  (python-shell-virtualenv-path . "/home/fakedrake/Projects/CSAIL/Python/py/")
+  (python-shell-virtualenv-path . "/home/fakedrake/Projects/CSAIL/Python/pypy/")
   (compile-command . "/home/fakedrake/Projects/CSAIL/Python/py/bin/pep8 /home/fakedrake/Projects/CSAIL/Python/WikipediaBase/wikipediabase /home/fakedrake/Projects/CSAIL/Python/WikipediaBase/tests")
   (fd-setup-test-cmd . "test")))
