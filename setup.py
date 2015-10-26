e3edd011550423e073419291dd62ca9290a8463d
Alvaro Morales <alvarom@csail.mit.edu>
2015-10-26 16:50:25 -0400

    Return strings encoded into numeric HTML entities instead of UTF-8 encoded strings
    Modified the WikipediaBase server to return strings encoded with numeric HTML entities. As of Oct '15, we haven't figured out how to correctly decode UTF-8 strings in Allegro CLisp. The output of WikipediaBase is mangled by START. To prevent this, we're encoding into HTML entities as a temprary workaround (to be fixed before deploying to production). Added namedentities as a dependency.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'fuzzywuzzy',
         'hiredis',
         'lxml',
+        'namedentities',
         'overlay-parse',
         'redis',
         'requests',
--- wikipediabase/telnet.py
+++ wikipediabase/telnet.py
@@ -6,6 +6,8 @@ A simple socket server for WikipediaBase
 
 import SocketServer
 
+from namedentities import numeric_entities
+
 from wikipediabase.log import Logging
 
 
@@ -16,7 +18,14 @@ class TelnetHandler(SocketServer.StreamRequestHandler, Logging):
         self.log().info('Received request: %s', msg)
         answer = self.server.eval(msg)
         assert(isinstance(answer, unicode))  # TODO : remove for production
-        self.wfile.write(answer.encode('utf-8'))
+
+        # answer is a unicode string that needs to be encoded
+        # As of Oct '15, Allegro Lisp doesn't correctly decode a utf-8 string.
+        # As a temporary workaround, we encode into numeric HTML entities
+        # e.g. "&#160;" for Unicode non-breaking space
+        # encoding to HTML entities is about 10x slower than encoding to utf-8
+        # TODO: use answer.encode('utf-8') instead of HTML entities
+        self.wfile.write(numeric_entities(answer))
 
 
 class TelnetServer(SocketServer.TCPServer):
