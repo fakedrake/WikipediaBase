c864b107e9d986b305542d035dc0b6454a814c22
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-11-06 12:33:36 +0200

    Configured metainfoboxes in tests

--- tests/test_infobox_scraper.py
+++ tests/test_infobox_scraper.py
@@ -13,36 +13,45 @@ try:
 except ImportError:
     import unittest
 
-import common
+from common import testcfg
 from wikipediabase.infobox_scraper import MetaInfobox
+import wikipediabase.fetcher as fetcher
 from wikipediabase.util import get_meta_infobox
 
 class TestMetaInfobox(unittest.TestCase):
 
     def setUp(self):
-        self.ibx = MetaInfobox('infobox_person')
+        self.ibx = MetaInfobox('infobox_person', configuration=testcfg)
 
     def test_newlines(self):
-        weapon = get_meta_infobox('Template:Infobox weapon').rendered_keys()
+        weapon = get_meta_infobox('Template:Infobox weapon',
+                                  configuration=testcfg).rendered_keys()
         self.assertIn('secondary_armament', weapon)
         self.assertEqual(weapon.get('secondary_armament'), "Secondary armament")
 
     def test_musician(self):
-        di = get_meta_infobox('Template:Infobox musical artist')
+        di = get_meta_infobox('Template:Infobox musical artist',
+                              configuration=testcfg)
+
+        self.assertIsInstance(di.fetcher, fetcher.StaticFetcher)
+        self.assertIn("origin", di.markup_source())
+        self.assertIn("Origin", di.html_source().text())
         self.assertIn("origin", di.rendered_keys())
         self.assertEqual(di.rendered_keys().get('origin'), "Origin")
 
     def test_regression_officeholder(self):
-        mibx = get_meta_infobox('Template:Infobox officeholder')
+        mibx = get_meta_infobox('Template:Infobox officeholder',
+                                configuration=testcfg)
         self.assertEqual("Died", mibx.rendered_keys().get("death_place"))
 
     def test_getting_by_symbol(self):
-        di = get_meta_infobox('Template:Infobox musical artist')
+        di = get_meta_infobox('Template:Infobox musical artist',
+                              configuration=testcfg)
         self.assertEqual(di.title(), "Infobox musical artist")
         self.assertEqual(di.symbol.url_friendly(), "Template:Infobox_musical_artist")
 
     def test_getting_by_title(self):
-        di = get_meta_infobox('Infobox musical artist')
+        di = get_meta_infobox('Infobox musical artist', configuration=testcfg)
         self.assertEqual(di.symbol.url_friendly(), "Template:Infobox_musical_artist")
         self.assertEqual(di.title(), "Infobox musical artist")
 
