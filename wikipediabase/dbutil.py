6649af8239140f5debff3ebbed3999d233b7c927
Alvaro Morales <alvarom@csail.mit.edu>
2015-11-21 18:16:03 -0500

    Automatically reconnect to DB if a query fails with an operational error
    Upgraded peewee to 2.7.3. Improved module organization in dbutil.py.
    Added missing dependencies to requirements.txt.

--- wikipediabase/dbutil.py
+++ wikipediabase/dbutil.py
@@ -2,13 +2,29 @@ import re
 
 from peewee import DoesNotExist, IntegerField, Model, TextField
 from playhouse.postgres_ext import PostgresqlExtDatabase, ServerSide
+from playhouse.shortcuts import RetryOperationalError
 
 from wikipediabase.util import encode
 
-LINK_PATTERN = re.compile(r'\*+.*?\[\[(.*?)\]\]')
-REDIRECT_PATTERN = re.compile(r"#REDIRECT:? ?\[\[(.*)?\]\]", flags=re.I)
+# -------
+# Database connection
+# -------
 
-db = PostgresqlExtDatabase('wikipediabase', user='wikipediabase', register_hstore=False)
+
+class PostgresqlExtRetryDatabase(RetryOperationalError, PostgresqlExtDatabase):
+
+    """
+    Automatically reconnect to the database and retry any queries that fail
+    with an OperationalError
+    """
+    pass
+
+db = PostgresqlExtRetryDatabase('wikipediabase', user='wikipediabase',
+                                register_hstore=False)
+
+# -------
+# Models
+# -------
 
 
 class Article(Model):
@@ -20,67 +36,9 @@ class Article(Model):
         database = db
         db_table = 'articles'
 
-def clean_symbol(symbol):
-    symbol = unicode(symbol)
-    symbol = symbol.replace(u'_', u' ')
-    symbol = symbol.split(u'|')[0]
-    symbol = symbol.split(u'#')[0]
-    return symbol
-
-
-def insert_article_batch(batch):
-    db.connect()
-    with db.atomic():
-        Article.insert_many(batch).execute()
-    db.close()
-
-
-def extract_redirect(markup):
-    redirect_match = re.search(REDIRECT_PATTERN, markup)
-    if redirect_match:
-        r = redirect_match.group(1)
-        return clean_symbol(r)
-    return None
-
-
-def is_disambiguation(title, markup):
-    """
-    Returns True if an article is a disambiguation page
-    """
-    if re.search(r'\(disamb\w+\)', title, flags=re.I):
-        # disambiguation page specified in the title
-        # e.g. "Anarchists (disambiguation)"
-        return True
-
-    # contains the {{disambiguation}} categories and sub-categories
-    # e.g {{disambig}} or {{disambig|latin}}
-    # see https://en.wikipedia.org/wiki/Template:Disambiguation for more info
-    disambig = r'\{\{[\w\|\-= ]*(disamb|geodis|hndis|numberdis)[\w\|\-= ]*\}\}'
-    if re.search(disambig, markup[-250:], flags=re.I):
-        return True
-
-    #if re.search(r"\s*?'{2,}\[*(.*?)\]*?'{2,}.*?refer\s*?to", markup[:250],
-    #             flags=re.I):
-    #    # matches phrases like 'may also refer to' and 'may refer to'
-    #    # the 250 character limit was chosen semi-arbitrarily to reduce false
-    #    # positives. Markup unfortunately doesn't stay consistent in
-    #    return True
-
-    return False
-
-
-def extract_disambiguation_links(markup):
-    links = []
-
-    # disambiguation pages may contain links to other articles under 'See also'
-    markup = re.split('== *See +also *==', markup, flags=re.I)[0]
-
-    for m in re.finditer(LINK_PATTERN, markup):
-        link = m.group(1)
-        link = clean_symbol(link)
-        links.append(link)
-
-    return links
+# -------
+# Data access methods
+# -------
 
 
 def all_articles():
@@ -89,6 +47,20 @@ def all_articles():
     """
     db.connect()
     return ServerSide(Article.select().tuples())
+
+
+def total_articles():
+    """
+    Returns the total number of articles
+    """
+    return Article.select().count()
+
+
+def insert_article_batch(batch):
+    db.connect()
+    with db.atomic():
+        Article.insert_many(batch).execute()
+    db.close()
 
 
 def get(symbol):
@@ -131,3 +103,67 @@ def get_markup(symbol):
     Raises a LookupError if the symbol is not found in the DB
     """
     return get_with_redirects(symbol).markup
+
+# -------
+# Markup utils
+# -------
+
+LINK_PATTERN = re.compile(r'\*+.*?\[\[(.*?)\]\]')
+REDIRECT_PATTERN = re.compile(r"#REDIRECT:? ?\[\[(.*)?\]\]", flags=re.I)
+
+
+def clean_symbol(symbol):
+    symbol = unicode(symbol)
+    symbol = symbol.replace(u'_', u' ')
+    symbol = symbol.split(u'|')[0]
+    symbol = symbol.split(u'#')[0]
+    return symbol
+
+
+def extract_redirect(markup):
+    redirect_match = re.search(REDIRECT_PATTERN, markup)
+    if redirect_match:
+        r = redirect_match.group(1)
+        return clean_symbol(r)
+    return None
+
+
+def is_disambiguation(title, markup):
+    """
+    Returns True if an article is a disambiguation page
+    """
+    if re.search(r'\(disamb\w+\)', title, flags=re.I):
+        # disambiguation page specified in the title
+        # e.g. "Anarchists (disambiguation)"
+        return True
+
+    # remove Template long comment
+    # see https://en.wikipedia.org/wiki/Template:Long_comment
+    long_comment = r"\{\{Short pages monitor\}\}\s*<!--.*?-->"
+    markup = re.sub(long_comment, '', markup, flags=re.I | re.S).strip()
+
+    # contains the {{disambiguation}} categories and sub-categories
+    # e.g {{disambig}} or {{disambig|latin}}
+    # see https://en.wikipedia.org/wiki/Template:Disambiguation for more info
+    # the 250 character limit was chosen semi-arbitrarily to reduce false
+    # positives. Some articles may contain a {{disambig|needed}} link in the
+    # article text
+    disambig = r'\{\{[\w\|\-= ]*(disamb|geodis|hndis|numberdis)[\w\|\-= ]*\}\}'
+    if re.search(disambig, markup[-250:], flags=re.I):
+        return True
+
+    return False
+
+
+def extract_disambiguation_links(markup):
+    links = []
+
+    # disambiguation pages may contain links to other articles under 'See also'
+    markup = re.split('== *See +also *==', markup, flags=re.I)[0]
+
+    for m in re.finditer(LINK_PATTERN, markup):
+        link = m.group(1)
+        link = clean_symbol(link)
+        links.append(link)
+
+    return links
