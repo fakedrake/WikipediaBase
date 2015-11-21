5d4e27503dd5db7dc2a7bc29d355c64e69ca65ca
Alvaro Morales <alvarom@csail.mit.edu>
2015-11-21 18:02:13 -0500

    Improve logging for generate symbols

--- scripts/generate_symbols_and_synonyms.py
+++ scripts/generate_symbols_and_synonyms.py
@@ -6,56 +6,63 @@ Create wikibase-term symbols and synonyms
 
 import logging
 import copy
+import time
 
 from unidecode import unidecode
 
 from wikipediabase.dbutil import all_articles, extract_redirect, \
-    extract_disambiguation_links, get, is_disambiguation
+    extract_disambiguation_links, get, is_disambiguation, total_articles
 from wikipediabase.symbols import is_good_title, get_synonyms, should_skip
 from wikipediabase.util import LRUCache, decode, encode
 
 logging.basicConfig(filename='symbols.log', level=logging.DEBUG)
 logging.getLogger("peewee").setLevel(logging.WARNING)
 
-symbols_f = open('wikibase-term-symbols', 'w')
-synonyms_f = open('wikibase-term-generated-synonyms', 'w')
+# TODO: change paths; pass in data_dir in sys.argv
+symbols_f = open('../data/wikibase-term-foo-symbols', 'w')
+synonyms_f = open('../data/wikibase-term-foo-generated-synonyms', 'w')
 
 latest_symbols = LRUCache(100)
 
 
 def write(symbol, synonyms):
     if is_good_title(symbol):
-        assert(isinstance(symbol, unicode))
         symbol = encode(symbol)
 
         if symbol not in latest_symbols:
+            # easy way to prevent writing (too) many duplicate symbols
             symbols_f.write('%s\n' % symbol)
             latest_symbols.set(symbol, symbol)
         for synonym in synonyms:
             if is_good_title(synonym):
-                # TODO: remove for production
-                assert(isinstance(synonym, unicode))
-
                 # TODO: remove when we upgrade to Guile 2.0
                 synonym = unidecode(synonym)
                 synonyms_f.write("{}\t{}\n".format(symbol, synonym))
 
 
 def generate(title, markup, trail):
+    """
+    Generate symbols and synonyms by recursively following redirects and
+    disambiguation pages.
+
+    trail is a set that stores the titles of articles visited in the
+    recursion path.
+    """
     if title in trail or should_skip(title):
         # already visited, circular loop
         return
 
     trail.add(title)
 
-    if len(trail) > 20:
-        logging.debug(' .. LONG TRAIL -> %s' % trail)
     logging.debug("  .. added %s to trail" % title)
+    if len(trail) > 20:
+        logging.debug(' .. long trail (%s) -> %s' % (len(trail), trail))
+
     redirect = extract_redirect(markup)
     disambig = is_disambiguation(title, markup)
 
     if not redirect and not disambig:
-        logging.debug("  .. base symbol, will write")
+        logging.debug("  .. found a symbol, will write")
         for t in trail:
             write(title, get_synonyms(t))
     else:
@@ -77,11 +84,34 @@ def generate(title, markup, trail):
 
 
 def main():
+    print "Calculating total number of articles in the DB"
+    total = total_articles()
+    print "Found %d articles in the DB" % total
+
+    count = 0.0
+    start = time.time()
+
     for page_id, title, markup in all_articles():
+        count += 1
         title = decode(title)
         logging.info("=== evaluating: %s" % title)
+
+        markup = get(encode(title)).markup
+
+        if count % 50000 == 0:
+            pct = count / total * 100.0
+            print "processed %s articles -- %s percent" % (count, pct)
+
         if not should_skip(title):
             generate(title, markup, set())
+
+
+    print "\nDONE"
+    print "time to process %s articles: %s" % (total, time.time() - start)
+
+    # TODO: call sort -u
+    symbols_f.close()
+    synonyms_f.close()
 
 if __name__ == '__main__':
     main()
